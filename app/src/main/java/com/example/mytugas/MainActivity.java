330a5cafaf7d81e0e0ff180bee3bc45c4f2c2866
gusrizalmuh <guslazir@gmail.com>
2021-10-11 07:28:36 +0700

    update layout intent

--- app/src/main/java/com/example/mytugas/MainActivity.java
+++ app/src/main/java/com/example/mytugas/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.mytugas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.mytugas.intent1;
+import com.example.mytugas.intent2;
+import com.example.mytugas.intent3;
+import com.example.mytugas.intent4;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView fdpizza, fddrink, fdhotdog, fdsoda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fdpizza = findViewById(R.id.fd_pizza);
+        fdpizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, intent1.class);
+                startActivity(intent);
+            }
+        });
+
+        fddrink = findViewById(R.id.fd_drink);
+        fddrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, intent2.class);
+                startActivity(intent);
+            }
+        });
+
+        fdhotdog = findViewById(R.id.fd_hotdog);
+        fdhotdog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, intent3.class);
+                startActivity(intent);
+            }
+        });
+
+        fdsoda = findViewById(R.id.fd_soda);
+        fdsoda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, intent4.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mytugas/intent2.java
@@ -0,0 +1,18 @@
+package com.example.mytugas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class intent2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent2);
+    }
+
+    public void drink(View view) {
+    }
+}
